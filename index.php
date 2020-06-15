5f2b12b4d3a7084877ed6c1b6291392ac85198f6
Marco <rubolini.marco@gmail.com>
2020-06-15 16:09:17 +0200

    Esercizio commentato

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
-<!-- Si tratta di un censuratore rudimentale: predisponete una variabile contenente un breve testo, poi tramite le funzioni sulle stringhe di php censurate una "badword" che viene passata in GET con *** (3 simboli di asterico).
-Stampate a schermo il testo censurato all'interno di un paragrafo e la lunghezza del paragrafo stesso. -->
-
-
 <?php
 
     // Predispongo una variabile contentente un testo
     $testo = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
-    var_dump($testo);
+    // var_dump($testo);
 
+    // Recupero la lunghezza del paragrafo
+    $lunghezza_paragrafo = strlen($testo);
+    //Stampo la lunghezza del paragrafo
+    echo $lunghezza_paragrafo;
+    // Recupero la badword dal parametro GET
     $badword = $_GET['badword'];
     // echo $badword;
-
+    // Sostituisco la parola badword da censurare con *** e la stampo a video
     echo str_replace($badword, '***', $testo);
 
 
